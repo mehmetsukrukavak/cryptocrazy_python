0fbe6a9103948c8a3d2878af0a054cae3a041391
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-12 10:36:09 +0300

    get data threading

--- asyncandthreadingexample.py
+++ asyncandthreadingexample.py
@@ -12,5 +12,32 @@ def get_data_sync(urls):
     print("Execution time: ", elapsed_time, " seconds")
     return json_array
 
+class ThreadingDownloader(threading.Thread):
+    json_array = []
+
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+
+    def run(self):
+        response = requests.get(self.url)
+        self.json_array = response.json()
+        return self.json_array
+
+def get_data_threading(urls):
+    st = time.time()
+    threads = []
+    for url in urls:
+        t = ThreadingDownloader(url)
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+        #print(t)
+    et = time.time()
+    elapsed_time = et - st
+    print("Execution time: ", elapsed_time, " seconds")
+
 urls = ["https://postman-echo.com/delay/3"] * 10
-get_data_sync(urls) #50
+#get_data_sync(urls) #50
+#get_data_threading(urls) #4.87
